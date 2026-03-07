432005f3f70c7197ccb104867eeccc5fac0d11a6
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-03-07 15:40:10 +0530

    LOGIN FORM STARTUP

--- Program.cs
+++ Program.cs
@@ -16,8 +16,7 @@ namespace Tax_Consultant_25
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            // Application.Run(new Form1()); 
+            Application.SetCompatibleTextRenderingDefault(false); 
             Application.Run(new frm_Login());
         }
     }
