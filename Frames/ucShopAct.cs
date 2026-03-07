ca715f68975a10eba9f23c8dd9928580f293cb2d
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-03-07 15:38:18 +0530

    Implemented Validations

--- Frames/ucShopAct.cs
+++ Frames/ucShopAct.cs
@@ -199,12 +199,26 @@ namespace Tax_Consultant_25.Frames
 
         private void txtClientName_Leave(object sender, EventArgs e)
         {
-            SearchClient();
+            if (txtClientName.Text == string.Empty)
+            {
+                return;
+            }
+            else
+            {
+                SearchClient();
+            }
         }
 
         private void txtTradeName_Leave(object sender, EventArgs e)
         {
-            SearchClient();
+            if (txtTradeName.Text == string.Empty)
+            {
+                return;
+            }
+            else
+            {
+                SearchClient();
+            }
         }
 
         private void btnSave_Click(object sender, EventArgs e)
