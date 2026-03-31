afab87f1f00d3dc8d184aa6e543437d0e6c58b85
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-03-31 23:34:10 +0530

    Minor Bugs FIxed

--- Frames/ucTDS.cs
+++ Frames/ucTDS.cs
@@ -504,9 +504,9 @@ namespace Tax_Consultant_25.Frames
         private void Clear()
         {
             DateTime today = DateTime.Now.Date;
-
-            dtpInputDate.Value = today;
             dtpDueDate.Value = today;
+            dtpInputDate.Value = today;
+            
 
             txtClientName.Clear();
             txtTaskName.Clear();
