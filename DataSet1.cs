8a2aa626594cb6f771872ad9c78b2e2d68609589
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-04-01 11:08:23 +0530

    Added New DataSet for Invoices

--- DataSet1.cs
+++ DataSet1.cs
@@ -1,47 +1,8 @@
 ﻿namespace Tax_Consultant_25
 {
 
-
     partial class DataSet1
     {
-        partial class dtClientsDataTable
-        {
-        }
 
-        partial class dtEmployeeDataTable
-        {
-        }
-
-        partial class dtPanTanDataTable
-        {
-        }
-
-        partial class dtTDSDataTable
-        {
-        }
-
-        partial class dtGSTDataTable
-        {
-        }
-
-        partial class dtBillsDataTable
-        {
-        }
-
-        partial class dtShopActDataTable
-        {
-        }
-
-        partial class dtPtecDataTable
-        {
-        }
-
-        partial class dtAccountingDataTable
-        {
-        }
-
-        partial class dtIncomeTaxDataTable
-        {
-        }
     }
 }
