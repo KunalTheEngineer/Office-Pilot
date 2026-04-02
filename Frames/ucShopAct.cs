7fba08740cf75cb0ecac50ea19e39372e5eaad63
KunalTheEngineer <kunalbthakare22@gmail.com>
2026-04-02 11:28:37 +0530

    Released v2.0

--- Frames/ucShopAct.cs
+++ Frames/ucShopAct.cs
@@ -435,9 +435,9 @@ namespace Tax_Consultant_25.Frames
                     query.ShowDialog();
                 }
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-                MessageBox.Show(ex.Message.ToString(), "UC_SHOPACT", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+               
             }
         }
 
